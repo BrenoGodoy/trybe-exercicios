24a98cc13bb3b3573a86ed4bb4cbbd14286b72e6
Breno Godoy <godoyy25z@gmail.com>
2022-02-03 14:52:36 -0300

    exercicios/4.1

---
+++ fundamentos/bloco-4-introduçao-ao-javascript-e-logica/dia-1/scripts.js
@@ -0,0 +1,14 @@
+let processoSeletivo;
+
+switch (processoSeletivo) {
+    case "aprovado":
+        console.log("aprovado")
+    case "lista":
+        console.log("lista")
+    case "reprovado":
+        console.log("reprovado")
+    default:
+        console.log("não se aplica")
+}
+
+
