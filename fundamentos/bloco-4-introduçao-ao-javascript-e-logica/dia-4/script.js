61b67444302e340b00e07d7f9af3c52c5c703144
Breno Godoy <godoyy25z@gmail.com>
2022-02-07 15:46:32 -0300

    segunda parte exercicio/4.4

--- fundamentos/bloco-4-introduçao-ao-javascript-e-logica/dia-4/script.js
+++ fundamentos/bloco-4-introduçao-ao-javascript-e-logica/dia-4/script.js
@@ -1,17 +1,20 @@
-let player = {
-    name: "Marta",
-    lastName: "Silva",
-    age: 34,
-    medals: {
-        golden: 2,
-        silver: 3,
-    },   
-    bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018],
-};
+let names = {
+    person1: 'João',
+    person2: 'Maria',
+    person3: 'Jorge',
+  };
 
+  for (let i in names) {
+      console.log("olá " + names[i]);
+  };
+// ------------------------------------------------------------------------------------------//
+  let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
+  };
 
-console.log("A jogadora " + player.name + " " + player.lastName + " tem " + player.age + " anos de idade.");
+  for (let i in car) {
+      console.log(i + " - " + car[i])
+  }
 
-console.log("A jogadora " + player.name + " " + player.lastName + " foi eleita a melhor do mundo " + player.bestInTheWorld.length + " vezes.");
-
-console.log("A jogadora " + player.name + " " + player.lastName + " possui " + player.medals.golden + " medalhas de ouro e " + player.medals.silver + " de prata.");
