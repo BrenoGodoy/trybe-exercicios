87af26dbc7cea4b5efd89c693bdd459e0a9383b5
Breno Godoy <godoyy25z@gmail.com>
2022-02-14 15:44:55 -0300

    Exercicios/5.4

--- fundamentos/bloco-5-dom-eventos-web-storage/dia-4/script.js
+++ fundamentos/bloco-5-dom-eventos-web-storage/dia-4/script.js
@@ -1,9 +1,40 @@
-console.log(localStorage.length); // não possui nada salvo, então o retorno é o valor: 0
-localStorage.setItem('firstname', 'Adam'); // salva uma entrada com a key = 'firstname' e value = 'Adam'
-localStorage.setItem('lastname', 'Smith'); // salva uma entrada com a key = 'lastname' e value = 'Smith'
-console.log(localStorage.getItem('lastname')); // retorna o valor 'Smith'
-console.log(localStorage.length); // possui duas entradas, então o retorno é o valor: 2
-localStorage.removeItem('lastname'); // remove a entrada referente a key = 'lastname'
-console.log(localStorage.length); // possui uma entrada, então o retorno é o valor: 1
-localStorage.clear(); // limpa todas as entradas salvas em localStorage
-console.log(localStorage.length); // não possui nada salvo, então o retorno é o valor: 0
+const button = document.getElementById('add-button');
+const input = document.getElementById('phrases-input');
+const list = document.getElementById('phrases-list');
+
+function insertPhraseInDOM() {
+  const phrasesList = JSON.parse(localStorage.getItem('phrases'));
+  const listLength = phrasesList.length - 1;
+  const phraseText = phrasesList[listLength];
+  const phrase = document.createElement('li');
+  phrase.innerText = phraseText;
+  list.appendChild(phrase);
+}
+
+function addPhraseToLocalStorage() {
+  const oldList = JSON.parse(localStorage.getItem('phrases'));
+  const phraseText = input.value;
+  oldList.push(phraseText);
+  localStorage.setItem('phrases', JSON.stringify(oldList));
+  insertPhraseInDOM();
+}
+
+function initialRenderization() {
+  if (localStorage.getItem('phrases') === null) {
+    localStorage.setItem('phrases', JSON.stringify([]));
+  } else {
+    const phrasesList = JSON.parse(localStorage.getItem('phrases'));
+    const listLength = phrasesList.length - 1;
+    for (let index = 0; index <= listLength; index += 1) {
+      const listElement = document.createElement('li');
+      listElement.innerText = phrasesList[index];
+      list.appendChild(listElement);
+    }
+  }
+}
+
+button.addEventListener('click', addPhraseToLocalStorage);
+
+window.onload = function() {
+  initialRenderization();
+};
