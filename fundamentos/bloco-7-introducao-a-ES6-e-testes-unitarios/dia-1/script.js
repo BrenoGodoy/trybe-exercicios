16ff47129129ed4311403dbda3cb12f24e6ac795
Breno Godoy <godoyy25z@gmail.com>
2022-03-03 15:46:05 -0300

    exercicios/7.1

---
+++ fundamentos/bloco-7-introducao-a-ES6-e-testes-unitarios/dia-1/script.js
@@ -0,0 +1,10 @@
+const nome = 'Breno'
+
+// Com o template literals
+console.log(`Primeira linha; ${nome}
+Segunda linha;
+Terceira linha;`
+);
+
+// Sem o template literals:
+console.log('Primeira linha;\n' + 'Segunda linha;\n' + 'Terceira linha;\n');
