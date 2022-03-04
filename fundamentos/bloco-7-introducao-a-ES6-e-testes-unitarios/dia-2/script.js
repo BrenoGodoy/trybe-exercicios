d0e027bcdf3e2fa4f7c9b0c0e6c749f29662ce50
Breno Godoy <godoyy25z@gmail.com>
2022-03-04 14:55:17 -0300

    exercicios/7.2

--- fundamentos/bloco-7-introducao-a-ES6-e-testes-unitarios/dia-2/script.js
+++ fundamentos/bloco-7-introducao-a-ES6-e-testes-unitarios/dia-2/script.js
@@ -1,23 +1,14 @@
-const student = {
-  Html: 'Muito Bom',
-  Css: 'Bom',
-  JavaScript: 'Ótimo',
-  SoftSkill: 'Ótimo',
+const países = {
+  França: 'Paris',
+  Brasil: 'Brasília',
+  Espanha: 'Madrid',
+  Portugal: 'Lisboa',
 };
+const pairKeyValue = Object.entries(países);
+console.log(pairKeyValue);
 
-const listSkillsValuesWithFor = (student) => {
-  const skills = [];
-  for(skill in student) {
-    skills.push(student[skill]);
-  }
-
-  return skills;
+for(index in pairKeyValue) {
+  console.log('--------');
+  console.log('País:', pairKeyValue[index][0]);
+  console.log('Capital:', pairKeyValue[index][1]);
 };
-
-const listSkillsValuesWithValues = (student) => Object.values(student);
-
-// Sem Object.values
-console.log(listSkillsValuesWithFor(student));
-
-// Com Object.values
-console.log(listSkillsValuesWithValues(student));
