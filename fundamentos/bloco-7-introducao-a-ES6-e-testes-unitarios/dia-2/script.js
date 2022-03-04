70dd5e9d1c6009c3c7f152a48bd77becf1b7ca87
Breno Godoy <godoyy25z@gmail.com>
2022-03-04 18:37:15 -0300

    exercicios/7.2 final

--- fundamentos/bloco-7-introducao-a-ES6-e-testes-unitarios/dia-2/script.js
+++ fundamentos/bloco-7-introducao-a-ES6-e-testes-unitarios/dia-2/script.js
@@ -1,23 +1,22 @@
-const person = {
-  name: 'Roberto',
-};
-
-const lastName = {
-  lastName: 'Silva',
-};
-
-const test = {
-  name: 'maria',
-  age: 23,
+function sum() {
+  if (!value1 || !value2) {
+    throw new Error('Faltam Valores!');
+  }
+  try {
+    const value1 = document.getElementById('value1').value;
+    const value2 = document.getElementById('value2').value;
+    let result = parseInt(value1) + parseInt(value2);
+  
+    document.getElementById('result').innerHTML = `Resultado: ${result}`;
+  } catch (error) {
+    alert(`ERRO: ${error.message}`);
+  } finally {
+    document.getElementById('value1').value = '';
+    document.getElementById('value2').value = '';
+  }
 }
 
-const clone = Object.assign(person, lastName);
-
-console.log(clone); // { name: 'Roberto', lastName: 'Silva' }
-console.log(person); // { name: 'Roberto', lastName: 'Silva' }
-
-Object.assign(clone, test);
-
-console.log(clone);
-console.log(person);
-console.log(test);
+window.onload = () => {
+  const button = document.getElementById('button');
+  button.addEventListener('click', sum);
+}
