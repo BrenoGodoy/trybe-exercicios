3d7fd6064237118b83104ef70fc6fccf7ecf5c6a
Breno Godoy <godoyy25z@gmail.com>
2022-03-04 14:52:16 -0300

    exercicios/7.2

--- fundamentos/bloco-7-introducao-a-ES6-e-testes-unitarios/dia-2/script.js
+++ fundamentos/bloco-7-introducao-a-ES6-e-testes-unitarios/dia-2/script.js
@@ -1,27 +1,23 @@
-function show(object) {
-  const keys = Object.keys(object);
-  const values = Object.values(object);
-  let show = `Estudante:`
-  for (const index in keys) {
-    show =`${show} 
-    ${keys[index]} Nível: ${values[index]}`
-  }
-  return show;
-}
-
-const student1 = {
+const student = {
   Html: 'Muito Bom',
   Css: 'Bom',
   JavaScript: 'Ótimo',
-  SoftSkills: 'Ótimo',
+  SoftSkill: 'Ótimo',
 };
 
-const student2 = {
-  Html: 'Bom',
-  Css: 'Ótimo',
-  JavaScript: 'Ruim',
-  SoftSkills: 'Ótimo',
-  Git: 'Bom', // chave adicionada
+const listSkillsValuesWithFor = (student) => {
+  const skills = [];
+  for(skill in student) {
+    skills.push(student[skill]);
+  }
+
+  return skills;
 };
 
-console.log(show(student1));
+const listSkillsValuesWithValues = (student) => Object.values(student);
+
+// Sem Object.values
+console.log(listSkillsValuesWithFor(student));
+
+// Com Object.values
+console.log(listSkillsValuesWithValues(student));
