7a7e50099a0200febe7fb45b0a07668960c465e5
Breno Godoy <godoyy25z@gmail.com>
2022-03-04 14:24:45 -0300

    exercicios/7.2

--- fundamentos/bloco-7-introducao-a-ES6-e-testes-unitarios/dia-2/script.js
+++ fundamentos/bloco-7-introducao-a-ES6-e-testes-unitarios/dia-2/script.js
@@ -1,16 +1,20 @@
-const verifyIsNumber = (value1, value2) => {
-    if (typeof value1 !== 'number' || typeof value2 !== 'number') {
-      throw new Error('Os valores devem ser numéricos');
-    }
-  };
-  
-  const sum = (value1, value2) => {
-    try {
-      verifyIsNumber(value1, value2);
-      return value1 + value2;
-    } catch (error) {
-      throw error.message;
-    }
-  };
-  
-  console.log(sum('2', 3));
+const customer1 = {
+  firstName: 'Roberto',
+  age: 22,
+  job: 'Teacher',
+};
+
+console.log(customer1);
+
+customer1.lastName = 'Faria';
+console.log(customer1);
+
+const customer2 = {
+  firstName: 'Maria',
+  age: 23,
+  job: 'Medic',
+};
+
+console.log(customer2);
+customer2['lastName'] = 'Silva';
+console.log(customer2);
