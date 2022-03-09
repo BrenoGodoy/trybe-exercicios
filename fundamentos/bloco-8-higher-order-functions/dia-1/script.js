d5f84328ba27db22a7d80aa139c17cf6ac77d401
Breno Godoy <godoyy25z@gmail.com>
2022-03-09 15:15:23 -0300

    exercicios/8.1

--- fundamentos/bloco-8-higher-order-functions/dia-1/script.js
+++ fundamentos/bloco-8-higher-order-functions/dia-1/script.js
@@ -1,9 +1,19 @@
-const acordando = () => 'acordando!!';
-const tomarCafe = () => 'Bora tomar café!!';
-const partiuDormir = () => 'Partiu dormir!!';
+const newEmployees = (func) => {
+  const employees = {
+    id1: func('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: func('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: func('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+  }
+  return employees;
+};
 
-const doingThings = (func) => func();
+const criaPesssoa = (nome) => {
+  const email = `${nome}@gmail.com`;
+  const emailRefact = email.split(' ').join(''); //retira os espaços.
+  return {
+    nomeCompleto: `${nome}`,
+    email: emailRefact,
+  };
+}
 
-console.log(doingThings(acordando));
-console.log(doingThings(tomarCafe));
-console.log(doingThings(partiuDormir));
+console.log(newEmployees(criaPesssoa));
