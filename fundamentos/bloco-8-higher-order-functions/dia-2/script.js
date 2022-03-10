73c2319028067175c16b9be605f9a4785ef3c594
Breno Godoy <godoyy25z@gmail.com>
2022-03-10 18:21:28 -0300

    exercicios/8.2

--- fundamentos/bloco-8-higher-order-functions/dia-2/script.js
+++ fundamentos/bloco-8-higher-order-functions/dia-2/script.js
@@ -2,6 +2,4 @@ const numbers = [19, 21, 30, 3, 45, 22, 15];
 
 const teste = (number) => number % 3 === 0 && number % 5 === 0;
 
-const div = numbers.find(teste);
-
-console.log(div);
+console.log(numbers.find(teste));
