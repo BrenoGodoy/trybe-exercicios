981d14aeb5c4f1e6b115c424989ee94402a4d495
Breno Godoy <godoyy25z@gmail.com>
2022-03-10 14:46:19 -0300

    exercicios/8.2

--- fundamentos/bloco-8-higher-order-functions/dia-2/script.js
+++ fundamentos/bloco-8-higher-order-functions/dia-2/script.js
@@ -1,14 +1,7 @@
-const emailListInData = [
-    'roberta@email.com',
-    'paulo@email.com',
-    'anaroberta@email.com',
-    'fabiano@email.com',
-  ];
-  
-  const showEmailList = (email) => {
-    console.log(`O email ${email} esta cadastrado em nosso banco de dados!`);
-  };
-  
-  // Adicione seu código aqui
- emailListInData.forEach(showEmailList);
- 
+const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+const teste = (number) => number % 3 === 0 && number % 5 === 0;
+
+const div = numbers.find(teste);
+
+console.log(div);
