59645b4ff73328c99f1b2f7be87ac6db20e367d4
Breno Godoy <godoyy25z@gmail.com>
2022-03-15 14:36:40 -0300

    exercicios/8.5

--- fundamentos/bloco-8-higher-order-functions/dia-5/script.js
+++ fundamentos/bloco-8-higher-order-functions/dia-5/script.js
@@ -1,11 +1,19 @@
-// Faça uma lista com as suas frutas favoritas
-const specialFruit = ['pêssego', 'manga', 'morango'];
-
-// Faça uma lista de complementos que você gostaria de adicionar
-const additionalItens = ['leite condensado', 'granola', 'creme de leite com açucar'];
-
-const fruitSalad = (fruit, additional) => {
-  return [...fruit, ...additional]; //spread operator
+// definindo o objeto
+const character = {
+  name: 'Luke SkyWalker',
+  age: '53',
+  description: {
+    specieName: 'Human',
+    jedi: true,
+  },
+  homeWorld: {
+    name: 'Tatooine',
+    population: '200000',
+  },
 };
 
-console.log(fruitSalad(specialFruit, additionalItens));
+// desestruturando o objeto:
+const { name, age, homeWorld: { name: planeta }, description: { jedi } } = character;
+
+// imprimindo os valores:
+console.log(`Esse é o ${name}, ele tem ${age} anos, mora no planeta ${planeta} e, por incrível que possa parecer, ele ${jedi ? 'é um Jedi' : 'não é um Jedi'}.`);
