684877b21a00d1601eeb15ad2690295935ec00ba
Breno Godoy <godoyy25z@gmail.com>
2022-04-04 16:11:30 -0300

    exercicios/10.1

---
+++ frontend/bloco-1-introdução-ao-react/dia-1/estudo-app/src/Component.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class MeClass extends React.Component() {
+  render() {
+    return (
+      <div>
+        <h1>Breno Godoy</h1>
+        <p>Rock n Roll</p>
+      </div>
+    )
+  }
+}
+
+export default MeClass;
