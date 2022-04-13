1c9ee18c405c325f3c0642469cd0687ff52e2821
Breno Godoy <godoyy25z@gmail.com>
2022-04-13 15:00:35 -0300

    exercicios/11.2

---
+++ frontend/bloco-11-estado-eventos-e-formularios-em-react/dia-2/my-form-2.0/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Fieldset1 from './Components/Fieldset1';
+import Header from './Components/Header';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Fieldset1 />
+    </>
+  )
+}
+
+export default App;
